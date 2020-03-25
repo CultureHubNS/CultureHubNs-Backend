71d6c6c260608d1466165fb005d6ae4a8929975a
suncicakijac <kijacsuncica@gmail.com>
2020-03-25 17:19:00 +0100

    Photo Upload

--- Application/User/CurrentUser.cs
+++ Application/User/CurrentUser.cs
@@ -1,4 +1,5 @@
 
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Interfaces;
@@ -39,7 +40,7 @@ namespace Application.User
                 DisplayName = user.DisplayName,
                 Username = user.UserName,
                 Token = _jwtGenerator.CreateToken(user),
-                Image = null
+                Image = user.Photos.FirstOrDefault( x => x.IsMain)?.Url
             };
         }
     }
