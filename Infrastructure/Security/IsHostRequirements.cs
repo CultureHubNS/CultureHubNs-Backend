6e721c1a5c9d2c5dd2b7a2f06e570067779e633a
MilicaKaretic <milicakaretic1@gmail.com>
2020-03-26 19:20:33 +0100

    Edit fixed

--- Infrastructure/Security/IsHostRequirements.cs
+++ Infrastructure/Security/IsHostRequirements.cs
@@ -9,10 +9,10 @@ using Persistence;
 
 namespace Infrastructure.Security
 {
-    public class IsHostRequirement : IAuthorizationRequirement
+     public class IsHostRequirement : IAuthorizationRequirement
     {
     }
-
+ 
     public class IsHostRequirementHandler : AuthorizationHandler<IsHostRequirement>
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
@@ -22,26 +22,22 @@ namespace Infrastructure.Security
             _context = context;
             _httpContextAccessor = httpContextAccessor;
         }
-
+ 
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsHostRequirement requirement)
         {
-            if (context.Resource is AuthorizationFilterContext authContext)
-            {
-                var currentUserName = _httpContextAccessor.HttpContext.User?.Claims?.SingleOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
-
-                var activityId = Guid.Parse(authContext.RouteData.Values["id"].ToString());
-
-                var activity = _context.Activities.FindAsync(activityId).Result;
-
-                var host = activity.UserActivities.FirstOrDefault(x => x.IsHost);
-
-                if (host?.AppUser?.UserName == currentUserName)
-                    context.Succeed(requirement);
-            } else {
-                context.Fail();
-            }
-
+            var currentUserName = _httpContextAccessor.HttpContext.User?.Claims?.SingleOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+ 
+            var activityId = Guid.Parse(_httpContextAccessor.HttpContext.Request.RouteValues.SingleOrDefault(x => x.Key == "id").Value.ToString());
+ 
+            var activity = _context.Activities.FindAsync(activityId).Result;
+ 
+            var host = activity.UserActivities.FirstOrDefault(x => x.IsHost);
+ 
+            if (host?.AppUser?.UserName == currentUserName)
+                context.Succeed(requirement);
+ 
             return Task.CompletedTask;
         }
     }
+
 }
