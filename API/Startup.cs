bfd5533b92a00be70f570eb87d4dbb540f3ab0b2
MilicaKaretic <milicakaretic1@gmail.com>
2020-03-28 20:04:34 +0100

    SignalR, Comments and ChatHub added

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,6 +1,8 @@
 
 using System.Text;
+using System.Threading.Tasks;
 using API.Middleware;
+using API.SignalR;
 using Application.Activities;
 using Application.Interfaces;
 using AutoMapper;
@@ -45,12 +47,19 @@ namespace API
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                    policy
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithOrigins("http://localhost:3000")
+                    .AllowCredentials();
                 });
             });
 
             services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddAutoMapper(typeof(List.Handler));
+
+            services.AddSignalR();
+
             services.AddControllers(opt => 
             {
                 var policy = new AuthorizationPolicyBuilder().
@@ -89,6 +98,20 @@ namespace API
                         ValidateAudience = false,
                         ValidateIssuer = false
                     };
+                    opt.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context => 
+                        {
+                            var accessToken = context.Request.Query["access_token"];
+                            var path = context.HttpContext.Request.Path;
+                            if (!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/chat")))
+                            {
+                                context.Token = accessToken;
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
                 });
 
             services.AddScoped<IJwtGenerator, JwtGenerator>();
@@ -121,6 +144,7 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chat");
             });
             //app.UseMvc();
         }
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -20,6 +20,8 @@ namespace Persistence
 
         public DbSet<Photo> Photos { get; set;}
 
+        public DbSet<Comment> Comments { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
