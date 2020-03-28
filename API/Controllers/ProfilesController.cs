dbe191a02734f2122e4863282dd1119dae8fc825
MilicaKaretic <milicakaretic1@gmail.com>
2020-03-28 08:01:08 +0100

    Edit user bio added

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using Application.Profiles;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -11,5 +12,13 @@ namespace API.Controllers
         {
             return await Mediator.Send(new Details.Query{Username = username});
         }
+
+        [HttpPut]
+        public async Task<ActionResult<Unit>> Edit(Edit.Command command)
+        {
+            return await Mediator.Send(command);
+        }
+
+   
     }
 }
