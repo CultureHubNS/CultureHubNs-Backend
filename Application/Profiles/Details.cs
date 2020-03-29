c83b1c0daf5fc41429960f471c1afbf9c554bf27
suncicakijac <kijacsuncica@gmail.com>
2020-03-29 01:51:11 +0100

    Following/Follower feature completed

--- Application/Profiles/Details.cs
+++ Application/Profiles/Details.cs
@@ -1,9 +1,7 @@
-using System.Linq;
+
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace Application.Profiles
 {
@@ -16,25 +14,18 @@ namespace Application.Profiles
 
         public class Handler : IRequestHandler<Query, Profile>
         {
-            private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IProfileReader _profileReader;
+
+            public Handler(IProfileReader profileReader)
             {
-                _context = context;
+               _profileReader = profileReader;
+
             }
 
             public async Task<Profile> Handle(Query request, CancellationToken cancellationToken)
             {
-                
-                var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == 
-                request.Username);
-                return new Profile
-                {
-                    DisplayName = user.DisplayName,
-                    Username = user.UserName,
-                    Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                    Photos = user.Photos,
-                    Bio = user.Bio
-                };
+                return await _profileReader.ReadProfile(request.Username);
+               
             }
         }
     }
--- Application/Profiles/Profile.cs
+++ Application/Profiles/Profile.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using Domain;
 
 namespace Application.Profiles
@@ -10,6 +11,10 @@ namespace Application.Profiles
         public string Image { get; set; }
         public string Bio { get; set; }
         
+         [JsonPropertyName("following")]
+        public bool IsFollowed { get; set; }
+        public int FollowersCount { get; set; }
+        public int FollowingCount { get; set; }
         public ICollection<Photo> Photos { get; set; }
     }
 }
