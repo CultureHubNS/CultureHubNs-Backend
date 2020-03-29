2ed2dd5db3f86f3e85e01c03ffa05ae63ce0b4b6
MilicaKaretic <milicakaretic1@gmail.com>
2020-03-29 21:26:12 +0200

    Paging, Sorting and Filtering

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -15,9 +15,11 @@ namespace API.Controllers
 
 
         [HttpGet]
-        public async Task<ActionResult<List<ActivityDto>>> List()
+        public async Task<ActionResult<List.ActivitiesEnvelope>> List(int? limit, 
+        int? offset, bool isGoing, bool isHost, DateTime? startDate)
         {
-            return await Mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query(limit,
+                offset, isGoing, isHost, startDate));
         }
 
         [HttpGet("{id}")]
