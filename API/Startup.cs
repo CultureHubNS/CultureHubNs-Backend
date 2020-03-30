73f7b4e4e47b87529d6b94fae0e796cbe1da9ac8
MilicaKaretic <milicakaretic1@gmail.com>
2020-03-30 11:37:35 +0200

    Adding logout for token expiry

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.Text;
 using System.Threading.Tasks;
 using API.Middleware;
@@ -51,6 +52,7 @@ namespace API
                     policy
                     .AllowAnyHeader()
                     .AllowAnyMethod()
+                    .WithExposedHeaders("WWW-Authenticate")
                     .WithOrigins("http://localhost:3000")
                     .AllowCredentials();
                 });
@@ -97,7 +99,9 @@ namespace API
                         ValidateIssuerSigningKey = true,
                         IssuerSigningKey = key,
                         ValidateAudience = false,
-                        ValidateIssuer = false
+                        ValidateIssuer = false,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero
                     };
                     opt.Events = new JwtBearerEvents
                     {
